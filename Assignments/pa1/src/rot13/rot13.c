ee3b44bac2702e0109513fe26ff8acc3adb164c7
Het N. Patel <hnp41@cp.cs.rutgers.edu>
2024-09-23 09:54:13 -0400

    Modified rot13

--- Assignments/pa1/src/rot13/rot13.c
+++ Assignments/pa1/src/rot13/rot13.c
@@ -1,25 +1,27 @@
 #include <stdio.h>
-#include <ctype.h> //isalpha()
+#include <ctype.h> //isalpha() and islower() to check is character is in alphabet and is lower case
 
-void rot13(const char *str) {
-    char ch;
-    while ((ch = *str++)) {
-        if (isalpha(ch)) {
-            if(islower(ch)) {
-                putchar(((ch - 'a' + 13) % 26) + 'a');
-            } else {
-                putchar(((ch - 'A' + 13) % 26) + 'A');
+void rot13(char *str) { //char * points to first character in array of characters (string)
+//char str is a single character, char *str is pointer to first character in string
+    while (*str) { //loop through entire string (runs till \0)
+        if (isalpha(*str)) { //is character alphabetic
+            if(islower(*str)) { //is character lowercase
+                printf("%c", (*str - 'a' + 13) % 26 + 'a'); //lower case, shift 13 positions and wrap around
             }
-        } else {
-            putchar(ch);
+            else {
+                printf("%c", (*str - 'A' + 13) % 26 + 'A'); //shifts upper case letters
+            }
         }
+        else {
+            printf("%c", *str); //non alphabetic characters
+        }
+        str++; 
     }
-    putchar('\n');
+    printf("\n");
 }
 
 int main(int argc, char *argv[]) {
     if (argc != 2) {
-        fprintf(stderr, "Usage: %s <string>\n", argv[0]);
         return 1;
     }
 
