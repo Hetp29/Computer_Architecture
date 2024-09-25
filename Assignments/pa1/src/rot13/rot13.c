43af6e0661d00e6fbb9819cfe9abb31de4ab7a31
Het N. Patel <hnp41@cp.cs.rutgers.edu>
2024-09-25 11:22:37 -0400

    Completed sorta program of pa1

--- Assignments/pa1/src/rot13/rot13.c
+++ Assignments/pa1/src/rot13/rot13.c
@@ -1,19 +1,20 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <ctype.h> //isalpha() and islower() to check is character is in alphabet and is lower case
 
-void rot13(char *str) { //char * points to first character in array of characters (string)
+void rot13(char *str) { 
 //char str is a single character, char *str is pointer to first character in string
-    while (*str) { //loop through entire string (runs till \0)
-        if (isalpha(*str)) { //is character alphabetic
-            if(islower(*str)) { //is character lowercase
-                printf("%c", (*str - 'a' + 13) % 26 + 'a'); //lower case, shift 13 positions and wrap around
+    while (*str) { 
+        if (isalpha(*str)) { 
+            if(islower(*str)) { 
+                printf("%c", (*str - 'a' + 13) % 26 + 'a'); 
             }
             else {
-                printf("%c", (*str - 'A' + 13) % 26 + 'A'); //shifts upper case letters
+                printf("%c", (*str - 'A' + 13) % 26 + 'A'); 
             }
         }
         else {
-            printf("%c", *str); //non alphabetic characters
+            printf("%c", *str); 
         }
         str++; 
     }
--- Assignments/pa1/src/sorta/sorta.c
+++ Assignments/pa1/src/sorta/sorta.c
@@ -1,6 +1,43 @@
+//strcmp() is for string comparison and uses lexicographical ordering
+//based on ASCII comparisons, 'Z' comes before 'a'
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h> //strcmp()
 
-int main() {
-    printf("Hello World \n");
-    return 0;
+void sortStrings(char *arr[], int n) {
+    //sorting algorithm 
+    char *temp;
+    //bubble sort, largest element is placed in correct position (end of the array), then second largest, etc.
+    for(int i = 1; i < n; i++) {
+        for(int j = i + 1; j < n; j++) {
+            if(strcmp(arr[i], arr[j]) > 0) {
+                temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+
+    }
+
+
+}
+
+int main(int argc, char *argv[]) {
+    //argc is how many command lines were passed to program (including program name itself)
+    //argv[] is array of char pointers where each element points to one command-line argument
+    //argc: how many elements are in argv and how many strings need to be stored
+    //argv provides data to be sorted (array of strings
+    //argv[0] is name or path of the program and not one of the arguments provided by the user
+    if (argc == 1) {
+        printf("No words to be printed. \n");
+        exit(1);
+        return 0;
+    }
+
+    sortStrings(argv, argc);
+
+    for(int i = 1; i < argc; i++) {
+        printf("%s\n", argv[i]);
+    }
+
 }
