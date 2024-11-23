f323f868e5b07acd700c46c857f0114e19cd14ee
Het N. Patel <hetp0322@gmail.com>
2024-11-23 12:49:55 -0500

    Implemented data structures

--- Assignments/pa4/src/truthtable.c
+++ Assignments/pa4/src/truthtable.c
@@ -1,7 +1,22 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main() {
-    printf("Hello World \n");
+typedef enum {
+    AND, OR, NAND, NOR, XOR, NOT, PASS, DECODER, MULTIPLEXER
+} GateType;
 
-    return 0;
-}
+typedef struct {
+    GateType type;
+    int size;
+    int *params;
+} Gate;
+
+typedef struct {
+    int num_inputs;
+    int num_objects;
+    int num_vars;
+    int *input_vars;
+    int *output_vars;
+    Gate *gates;
+    int num_gates;
+} Circuit;
